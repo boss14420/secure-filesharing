347a041d6d3c8008e25da5bab46d0259a0d2fb08
boss14420 <firefoxlinux@gmail.com>
2014-12-22 20:54:52 +0700

    missing header

---
+++ src/include/config.hh
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  config.hh
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  12/21/2014 03:59:40 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+
+#ifndef __CONFIG_HH__
+#define __CONFIG_HH__
+
+#define USE_GMP
+#include <gmpxx.h>
+typedef mpz_class BigInt;
+
+#endif // __CONFIG_HH__
